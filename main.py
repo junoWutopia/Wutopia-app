78b2264305c5b5fb830098cafb79add7888d7a80
gpwaob92679 <gpwaob92679gp@gmail.com>
2022-10-01 12:46:08 +0800

    Finish download screen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import re
 import time
 
+import requests
 from bs4 import BeautifulSoup
 from kivy.lang import Builder
 from kivy.metrics import dp
@@ -19,38 +20,60 @@ from kivymd.uix.label import MDLabel
 from kivymd.uix.navigationdrawer import MDNavigationLayout
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.screenmanager import MDScreenManager
+from kivymd.uix.snackbar import Snackbar
 from kivymd.uix.spinner import MDSpinner
 
 from utils import *
 
+DOMAIN = 'https://www.missionjuno.swri.edu'
+
 
 class PreviewContent(MDBoxLayout):
-    def __init__(self):
-        super(PreviewContent, self).__init__()
+    def __init__(self, **kwargs):
+        super(PreviewContent, self).__init__(**kwargs)
         self.orientation = 'vertical'
         self.size_hint_y = None
-        # self.label = MDLabel(text='Please wait while the preview image is '
-        #                      'begin downloaded...',
-        #                      halign="center",)
+        self.label = MDLabel(text='Please wait while the preview image is '
+                             'begin downloaded...',
+                             halign="center")
         # self.spinner = MDSpinner(active=True,
         #                          size=(dp(50), dp(50)),
         #                          size_hint=(None, None),
         #                          pos_hint={'center_x': 0.5})
-        #
-        # self.add_widget(self.label)
+
+        self.add_widget(self.label)
         # self.add_widget(self.spinner)
 
-    def download_preview(self, res_url: str):
+    def download_preview(self, res_url: str, res_id: int):
         webpage = requests.get(res_url)
         soup = BeautifulSoup(webpage.text, 'html.parser')
 
         lazy_img = soup.find('div', {'class': 'lazy_img'}).find('img')
         print(lazy_img)
 
-        tmp = get_path('./tmp')
-        download(tmp / 'preview.png', requests.get(lazy_img['src']))
+        file_path = f'tmp/{res_id}_preview.jpg'
+        download('file', file_path, requests.get(lazy_img['src']))
+        self.label.text=''
+        self.add_widget(Image(source=file_path, size_hint_y=None))
 
-        self.add_widget(Image(source='tmp/preview.png', size_hint_y=None))
+
+class DownloadContent(MDBoxLayout):
+    def __init__(self, **kwargs):
+        super(DownloadContent, self).__init__(**kwargs)
+        self.orientation = 'vertical'
+        self.size_hint_y = None
+        # self.height = dp(100)
+        self.label = MDLabel(text='Please wait while the resource is being '
+                             'downloaded...\n'
+                             'This may take a while. The app may become '
+                             'unresponsive.',
+                             halign="center")
+        # self.spinner = MDSpinner(active=True,
+        #                          size=(dp(50), dp(50)),
+        #                          size_hint=(None, None),
+        #                          pos_hint={'center_x': 0.5})
+        self.add_widget(self.label)
+        # self.add_widget(self.spinner)
 
 
 class HomeScreen(MDScreen):
@@ -58,12 +81,11 @@ class HomeScreen(MDScreen):
 
 
 class DownloadScreen(MDScreen):
-
-    # download_enabled = BooleanProperty(False)
-
     def __init__(self, **kwargs):
         super(DownloadScreen, self).__init__(**kwargs)
         self.user_input = None
+        self.res_id = None
+        self.session = requests.Session()
         # print(self.ids.text_field)
         # self.ids.text_field.bind(
         #     on_text_validate=self.check_resource,
@@ -76,32 +98,69 @@ class DownloadScreen(MDScreen):
         self.user_input = self.ids.text_field.text
         print(self.user_input)
         pattern = re.compile('https:\/\/www.missionjuno.swri.edu\/junocam\/'
-                             'processing\?id=\d+')
+                             'processing\?id=(\d+)')
+        match = pattern.match(self.user_input)
 
-        if pattern.match(self.user_input):
+        if match:
+            self.res_id = match.group(1)
             self.ids.text_field.error = False
             self.ids.preview_resource_btn.disabled = False
             self.ids.download_resource_btn.disabled = False
         else:
+            self.res_id = None
             self.ids.text_field.error = True
             self.ids.preview_resource_btn.disabled = True
             self.ids.download_resource_btn.disabled = True
 
-    def preview_resource(self):
+    def show_preview_dialog(self):
         content = PreviewContent()
         ok_btn = MDFlatButton(text='OK')
         dialog = MDDialog(
             title='Preview',
             type='custom',
             content_cls=content,
-            buttons=[ok_btn]
+            buttons=[ok_btn],
+            on_open=self.preview_resource
         )
         ok_btn.bind(on_press=dialog.dismiss)
         dialog.open()
-        content.download_preview(self.user_input)
+        # content.download_preview(self.user_input, self.res_id)
 
-    def download_resource(self):
-        pass
+    def preview_resource(self, dialog):
+        dialog.content_cls.download_preview(self.user_input, self.res_id)
+
+    def show_download_dialog(self):
+        content = DownloadContent()
+        dialog = MDDialog(
+            title=f'Downloading...',
+            type='custom',
+            content_cls=content,
+            on_open=self.download_resource,
+            on_pre_dismiss=self.show_done_snackbar
+        )
+        dialog.open()
+
+    @staticmethod
+    def show_done_snackbar(dialog):
+        Snackbar(text='Done').open()
+
+    def download_resource(self, dialog):
+        r_page = self.session.get(self.user_input)
+        r_page_soup = BeautifulSoup(r_page.text, 'html.parser')
+        # print(r_page_soup.title)
+        processing = r_page_soup.find(
+            'div',
+            {'class':
+                 'processing_tools half stack_full textright padT_half right'}
+        )
+
+        for zip_tag in processing.find_all('a',
+                                           {'class': 'marR download_zip'}):
+            dl_url = DOMAIN + zip_tag['href']
+            download('dir', f'data/{self.res_id}',
+                     self.session.get(dl_url, stream=True))
+
+        dialog.dismiss()
 
 
 class EditScreen(MDScreen):
@@ -120,10 +179,11 @@ class WutopiaApp(MDApp):
         self.manager.add_widget(EditScreen(name='edit'))
         self.manager.transition = NoTransition()
 
+        self.data = get_path('./data')
+        self.tmp = get_path('./tmp')
+
     # def on_stop(self):
-    # self.manager.get_screen('download').destroy_browser()
-    #     self.browser_layout.browser_widget._browser.CloseBrowser(True)
-    #     del self.browser_layout.browser_widget._browser
+    #     self.tmp.unlink()
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,3 +1,7 @@
+import re
+from typing import Literal, Union
+
+
 from pathlib import Path
 import requests
 from tqdm import tqdm
@@ -10,11 +14,26 @@ def get_path(p) -> Path:
     return path
 
 
-def download(save_file: Path, response: requests.Response) -> None:
+def download(mode: Literal['dir', 'file'],
+             save_path: Union[str, Path],
+             response: requests.Response) -> None:
+    if isinstance(save_path, str):
+        save_path = Path(save_path)
+
+    filename_pattern = re.compile('(?<=filename=").*(?=")')
+
+    if mode == 'dir':
+        save_dir = get_path(save_path)
+        dl_header = response.headers['Content-Disposition']
+        filename = filename_pattern.search(dl_header).group(0)
+    else:
+        save_dir = get_path(save_path.parent)
+        filename = save_path.name
+
     with tqdm.wrapattr(
-            open(save_file, 'wb'),
+            open(save_dir / filename, 'wb'),
             'write',
-            desc=save_file.name,
+            desc=filename,
             total=int(response.headers['content-length']),
             miniters=1,
     ) as f:
